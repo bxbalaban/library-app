718ee3bf82a6ea191f34a3f6a4cae630c48f148f
bxbalaban <balabanbsr@gmail.com>
2022-07-20 16:07:40 +0200

    init

---
+++ src/pages/formPage.tsx
@@ -0,0 +1,11 @@
+
+
+const Form=()=> {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+     Form
+    </h1>
+  );  
+}
+
+export default Form;
---
+++ src/pages/mainPage.tsx
@@ -0,0 +1,11 @@
+
+
+const Main=()=> {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+     Main
+    </h1>
+  );  
+}
+
+export default Main
