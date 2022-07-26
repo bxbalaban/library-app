91501bf6a16911d2bdc4eaf5ce2f82539e834a71
bxbalaban <balabanbsr@gmail.com>
2022-07-26 14:06:49 +0200

    footer component started

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import './global.css';
 import Form from './pages/formPage'
 import Navbar from "./pages/navbar"
 import Main from "./pages/mainPage"
+import Footer from "./pages/footer"
 
 
 const App=()=> {
   return (
-      <div>
+      <div style={{backgroundColor:"white"}}>
         <Navbar></Navbar>
         <Main></Main>
         <Form></Form>
+        <Footer></Footer>
       </div>
   );  
 }
---
+++ src/pages/footer.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import "../components/style-footer.css"
+import {
+    FaFacebook,
+    FaInstagram,
+    FaTwitter,  
+}from "react-icons/fa"
+
+const Footer = () => {
+    return (
+  
+      <div className='footer-container-1'>
+        <div className='footer-container-2'>
+            <div>
+                <h6 className='footer-text-1'>What We Offer</h6>
+                <ul>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                </ul>
+            </div>
+            <div>
+                <h6 className='footer-text-1'>Support</h6>
+                <ul>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                </ul>
+            </div>
+            <div>
+                <h6 className='footer-text-1'>Documentation</h6>
+                <ul>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                </ul>
+            </div>
+            <div>
+                <h6 className='footer-text-1'>About</h6>
+                <ul>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                    <li className='footer-list'>Lorem</li>
+                </ul>
+            </div>
+        </div>
+      </div>
+  
+    );
+  }
+  
+  export default Footer
+  
