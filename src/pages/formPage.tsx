d60edb16bf928775ea5a6c7297ac144d6a549ceb
bxbalaban <balabanbsr@gmail.com>
2022-08-02 19:21:13 +0200

    button submit added

--- src/pages/formPage.tsx
+++ src/pages/formPage.tsx
@@ -2,60 +2,78 @@
 import "../components/style-form.css"
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Routes, Route, useNavigate,Link } from 'react-router-dom';
 import Form1 from "./form1";
 import Form2 from "./form2";
+import Output from "./outputPage";
 
 import { Button } from "react-scroll";
-import { BADHINTS } from "dns";
+
 import { getAllByDisplayValue } from "@testing-library/react";
 const Form = () => {
 
-  const [count, setCount] = useState(0)
-  let [back, setBack] = useState(false);
+  const [count, setCount] = useState(1)
+  const [btnText, setBtnText] = useState("Next");
 
+  const navigate = useNavigate();
+
+  const navigateToOutput = () => {
+    // 👇️ navigate to /output
+    navigate('/output');
+  };
   const handleClick = (param: string) => {
     if (param === "b" && count > 0) {
       setCount(count - 1)
     }
-    else if (param === "n" && count < 3) {
+    else if (param === "n" && count < 4) {
       setCount(count + 1)
     }
 
-    if (count <= 0) {
-      document.getElementById("btn-back")?.setAttribute("class", "hide-btn")
-      document.getElementById("btn-next")?.setAttribute("class", "show-btn")
-    }
-    else if (count > 0 && count<= 3) {
-      document.getElementById("btn-back")?.setAttribute("class", "show-btn")
-      document.getElementById("btn-next")?.setAttribute("class", "show-btn")
-      switch(count){
+    if (count >= 0 && count <= 3) {
+      switch (count) {
+        case 0:
+          {
+            document.getElementById("form1")?.setAttribute("class", "show-form")
+            document.getElementById("form2")?.setAttribute("class", "hide-form")
+            document.getElementById("btn-back")?.setAttribute("class", "hide-btn")
+            document.getElementById("btn-next")?.setAttribute("class", "show-btn")
+            break;
+          }
         case 1:
-        {
-          console.log("case 1: has been entered")
-          document.getElementById("form1")?.setAttribute("class","show-form")
-          document.getElementById("form2")?.setAttribute("class","hide-form")
-          break;
-        }
+          {
+            document.getElementById("form1")?.setAttribute("class", "hide-form")
+            document.getElementById("form2")?.setAttribute("class", "show-form")
+            document.getElementById("btn-back")?.setAttribute("class", "show-btn")
+            break;
+          }
         case 2:
           {
-            console.log("case 2: has been entered")
-            document.getElementById("form1")?.setAttribute("class","hide-form")
-          document.getElementById("form2")?.setAttribute("class","show-form")
-          break;
+            document.getElementById("form1")?.setAttribute("class", "show-form")
+            document.getElementById("form2")?.setAttribute("class", "show-form")
+            document.getElementById("btn-next")?.setAttribute("class", "show-btn")
+            setBtnText("Next")
+            break;
           }
-        case 2: 
-          {break;}
         case 3:
-          {break;}
+          { //the last page print 
+            document.getElementById("form1")?.setAttribute("class", "hide-form")
+            document.getElementById("form2")?.setAttribute("class", "hide-form")
+            document.getElementById("btn-next")?.setAttribute("class", "submit-btn")
+            setBtnText("Submit")
+            break;
+          }
+        case 4:
+          { //go to the output page
+           
+            
+            break;
+          }
         default:
-          {break;}
-      }
-      
+          {
 
-    }
-    else if (count > 3) {
-      document.getElementById("btn-next")?.setAttribute("class", "hide-btn")
-      document.getElementById("btn-back")?.setAttribute("class", "show-btn")
+            break;
+          }
+      }
     }
     console.log(count)
   }
@@ -87,16 +105,16 @@ const Form = () => {
         <div className="form-text-container">
           <div className="form-text-md">Form.</div>
         </div>
-        <div id="form1">
+        <div id="form1" className="show-form">
           <Form1></Form1>
         </div>
-        <div id="form2">
+        <div id="form2" className="hide-form">
           <Form2></Form2>
         </div>
-       
+
         <div className="button-align ">
-          <button id="btn-back" className="button hide" onClick={() => handleClick("b")} > Back</button>
-          <button id="btn-next" className="button" onClick={() => handleClick("n")}> Next</button>
+          <button id="btn-back" className="hide-btn" onClick={() => handleClick("b")} > Back</button>
+          <button id="btn-next" className="show-btn" onClick={() => handleClick("n")}> {btnText}</button>
         </div>
         {/* TODO -- fix this tailwind css code  */}
       </div>
--- src/pages/outputPage.tsx
+++ src/pages/outputPage.tsx
@@ -1,11 +1,17 @@
 
+import Footer from "./footer";
+import Navbar from "./navbar";
 
-const Output=()=> {
+const Output = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-     Output
-    </h1>
-  );  
+    <div>
+      <Navbar></Navbar>
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Output
+      </h1>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default Output
